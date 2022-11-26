41602d34e220bbaaa39d99aa8d2f579f1b9a23a9
drupad c s <csdrupad0@gmail.com>
2022-11-26 12:47:55 +0530

    second commit

--- class_es.py
+++ class_es.py
@@ -112,11 +112,11 @@
 
 # staticmethod
 
-class Math:
-
-    @staticmethod
-    def add5(x):
-        return x + 5
-
-
-print(Math.add5(5))
+# class Math:
+#
+#     @staticmethod
+#     def add5(x):
+#         return x + 5
+#
+#
+# print(Math.add5(5))
